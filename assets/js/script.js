42c3cdf999a48364d9bd61e6f7f18f0c2bcaf33b
Cauã Gustavo <caua.g.silva16@aluno.senai.br>
2026-05-14 11:01:59 -0300

    MOD: Começando arquivo JS. Estilização melhorada, recriação da aba galeria.

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('alertSucesso').addEventListener('click', function() {
+    Swal.fire('Matricula foi enviada com sucesso!', 
+      'Compareça a uma de nossas unidades presenciais para finalizar o cadastro', 
+      'success');
+  });
